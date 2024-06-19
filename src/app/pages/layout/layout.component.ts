f25fa7d6b2451093bea6a99387d1ba430bc2503e
A Venkatesh <avenkateshanand1998@gmail.com>
2024-06-19 20:51:42 +0530

    Here I Have Develop the Login page Functionality for the login page

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-
+   
+  // router = inject(Router);
+constructor(private router: Router){}
+  onLogOff() {
+    localStorage.removeItem('ticketUser');
+    this.router.navigateByUrl('login');
+  }
 }
