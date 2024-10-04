340fb97e759ac2999dddf8bfcb4001c0067d5eb7
A Venkatesh <avenkateshanand1998@gmail.com>
2024-10-04 23:01:13 +0530

    I Have Committing Defferent Types of Deportments Here

--- src/app/pages/department/department.component.ts
+++ src/app/pages/department/department.component.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MasterService } from '../../service/master.service';
+import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-department',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, DatePipe],
   templateUrl: './department.component.html',
   styleUrl: './department.component.css'
 })
-export class DepartmentComponent {
+export class DepartmentComponent implements OnInit{
+ 
+  masterSrc= inject(MasterService);
+  deptList: any[]=[];
+  newDeptObj: any = {
+    "deptId": 0,
+    "deptName": "",
+    "createdDate": ""
+  }
 
+  ngOnInit(): void {
+    this.getDept();
+  }
+
+  getDept() {
+    this.masterSrc.getAllDept().subscribe((res:any)=>{
+      debugger;
+      this.deptList = res.data;
+    })
+  }
+
+  saveDept() {
+    debugger;
+    this.masterSrc.createNewDept(this.newDeptObj).subscribe((res:any)=>{
+      debugger;
+      if(res.result) {
+        alert("Dept Created Success");
+        this.getDept();
+        this.newDeptObj = {
+          "deptId": 0,
+          "deptName": "",
+          "createdDate": "",
+        }
+        }else {
+        alert(res.message)
+      }
+    }) 
+  }
+  onEdit(data: any) {
+    this.newDeptObj = data;
+  }
+  updateDept() {
+    debugger;
+    this.masterSrc.updateDept(this.newDeptObj).subscribe((res:any)=>{
+      debugger;
+      if(res.result) {
+        alert("Dept Updated Success");
+        this.getDept();
+        this.newDeptObj ={
+          "deptId": 0,
+          "deptName": "",
+          "createdDate": "",
+        }
+      } else {
+        alert(res.message)
+      }
+    }) 
+  }
+  onDelete(id: number) {
+    const isDelete = confirm("Are you sure want Delete");
+    if(isDelete) {
+      this.masterSrc.deleteDeptById(id).subscribe((res:any)=>{
+        debugger;
+        if(res.result) {
+          alert("Dept Deleted Success");
+          this.getDept();
+        } else {
+          alert(res.message)
+        }
+      }) 
+    }
+  }
 }
+
